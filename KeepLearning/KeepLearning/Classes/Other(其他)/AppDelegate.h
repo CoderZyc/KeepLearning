fcc81ab41f12b9a9117f66d15ec421d4bd58e6e3
zyc <941077044@qq.com>
2016-03-20 15:21:54 +0800

    初始化项目

---
+++ KeepLearning/KeepLearning/Classes/Other(其他)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeepLearning
+//
+//  Created by zyc on 16/3/20.
+//  Copyright © 2016年 zyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
